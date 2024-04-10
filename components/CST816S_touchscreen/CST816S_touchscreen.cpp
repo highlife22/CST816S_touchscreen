9b605d97048f19a23a377ee0e9085af84f7af7af
highlife22 <1440980+highlife22@users.noreply.github.com>
2024-04-10 19:09:02 +0200

    Update CST816S_touchscreen.cpp

--- components/CST816S_touchscreen/CST816S_touchscreen.cpp
+++ components/CST816S_touchscreen/CST816S_touchscreen.cpp
@@ -29,6 +29,7 @@ void CST816STouchScreen::loop() {
     unsigned long currentMillis = millis();
 
     if (touch.available()) {
+        ESP_LOGI("touchscreen", "pos: %s", touch.data.gestureID);
         if(touch.data.gestureID != 0 && touch.data.gestureID != 5 ){                          //ignore None gesture type
             if(currentMillis - previousMillis > interval) {     //debounce
                 char buf[20];
